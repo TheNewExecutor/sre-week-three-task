8d38ed7689e2fb61de3dc5a4478c9077d412976f
Jonathan <jrsarker@ucdavis.edu>
2024-04-24 04:50:41 +0000

    Update watcher bash script
    Wrote a watcher script to keep track of restart counter to trigger
    a scale down of replicas

--- watcher.sh
+++ watcher.sh
@@ -4,9 +4,14 @@ DEPLOYMENT_NAME='swype-app'
 MAX_RESTARTS=5
 restarts='kubectl get pods -n ${NAMESPACE} -l app=${DEPLOYMENT_NAME} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}"'
 
-while [[ $restarts -le $MAX_RESTARTS ]]; do 
+while true; do 
    echo "Current number of restarts: ${restarts}"
+   
+   if (( ${restarts} > MAX_RESTARTS)); then
+       echo "Max number of restarts exceeded. Scaling down deployment..."
+       kubectl scale --replicas=0 deployment/${DEPLOYMENT_NAME} -n ${NAMESPACE}
+       break
+   fi
+
    sleep 60
 done 
-echo "Max number of restarts exceeded. Scaling down deployment..."
-kubectl scale --replicas=0 deployment/${DEPLOYMENT_NAME} -n ${NAMESPACE}
