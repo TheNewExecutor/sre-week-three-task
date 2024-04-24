959c3409f2b5fdbf56764f4423188a00cca75977
Jonathan <jrsarker@ucdavis.edu>
2024-04-24 04:43:41 +0000

    Update watcher script
    Modified watcher script to

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,12 @@
 #TODO
+NAMESPACE='sre'
+DEPLOYMENT_NAME='swype-app'
+MAX_RESTARTS=5
+restarts='kubectl get pods -n ${NAMESPACE} -l app=${DEPLOYMENT_NAME} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}"'
+
+while [[ $restarts -le $MAX_RESTARTS ]]; do 
+   echo "Current number of restarts: ${restarts}"
+   sleep 60
+done 
+echo "Max number of restarts exceeded. Scaling down deployment..."
+kubectl scale --replicas=0 deployment/${DEPLOYMENT_NAME} -n ${NAMESPACE}
