7e65f52f5bb6b8f8abdf67bf8fe61c670d10512d
Jonathan <jrsarker@ucdavis.edu>
2024-04-24 04:56:52 +0000

    Updated watcher script closer to answer key
    The script was not displaying restarts so this change is an
    attempt to remedy this in case some expressions were incorrect.

--- watcher.sh
+++ watcher.sh
@@ -2,12 +2,12 @@
 NAMESPACE='sre'
 DEPLOYMENT_NAME='swype-app'
 MAX_RESTARTS=5
-restarts='kubectl get pods -n ${NAMESPACE} -l app=${DEPLOYMENT_NAME} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}"'
 
 while true; do 
-   echo "Current number of restarts: ${restarts}"
-   
-   if (( ${restarts} > MAX_RESTARTS)); then
+   RESTARTS=$(kubectl get pods -n ${NAMESPACE} -l app=${DEPLOYMENT_NAME} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}")
+
+   echo "Current number of restarts: ${RESTARTS}"
+   if (( RESTARTS > MAX_RESTARTS )); then
        echo "Max number of restarts exceeded. Scaling down deployment..."
        kubectl scale --replicas=0 deployment/${DEPLOYMENT_NAME} -n ${NAMESPACE}
        break
